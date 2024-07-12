33ae373b351f6faae14c2734ac8e83c3c95f2943
anit87 <anit87@bwsteam.com>
2024-07-12 17:50:53 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,13 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+export const metadata = {
+  title: 'Acme',
+  openGraph: {
+    title: 'Acme',
+    description: 'Acme is a...',
+  },
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`} >{children}</body>
     </html>
   );
 }
